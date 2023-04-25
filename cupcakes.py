515e63096e4af6fbcefa66373315ed4103a65d74
Sarah Burtenshaw <sarahburtenshaw@Sarahs-MacBook-Air.local>
2023-04-25 17:33:18 -0600

    Finished part 5, fixed issues that Chalon pointed out

--- cupcakes.py
+++ cupcakes.py
@@ -115,5 +115,15 @@ def get_cupcakes(file):
         reader = list(reader)
         return reader
 
-
+def find_cupcake(file, name):
+    for cupcake in get_cupcakes(file):
+        if cupcake["name"] == name:
+            return cupcake
+        return None
+    
+def add_cupcake_dictionary(file, cupcake):
+    with open(file, "a", newline="\n") as csvfile:   
+        fieldnames = ["size", "name", "price", "flavor", "frosting", "sprinkles", "filling"]
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+         
 print("CSV has created a new file")
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from cupcakes import get_cupcakes
+from flask import Flask, render_template, url_for, redirect
+from cupcakes import get_cupcakes, find_cupcake, add_cupcake_dictionary
 app = Flask(__name__)
 
 @app.route("/endpoint")
@@ -15,9 +15,28 @@ def home():
 def all_cupcakes():
     return render_template("individual-cupcake.html")
 
+@app.route("/cupcake_individual/<name>")
+def individual_cupcake(name):
+    cupcake = find_cupcake("sample.csv", name)
+    
+    if cupcake:
+        return render_template("individual-cupcake.html", cupcake=cupcake)
+    else:
+        return "Sorry cupcake not found"
+
 @app.route("/order")
 def order():
     return render_template("order.html")
+
+@app.route("/add-cupcake/<name>")
+def add_cupcake(name):
+    cupcake = find_cupcake("cupcakes.csv", name)
+    
+    if cupcake:
+        add_cupcake_dictionary("orders.csv", cupcake)
+        return redirect(url_for("home"))
+    else:
+        return "Sorry cupcake not found."
 
 if __name__ == "__main__":
     app.debug = True
